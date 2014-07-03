d6f62b5637488d767def8b5b41ec964382ad0bf1
Luciano Leggieri <lleggieri@atlassian.com>
2014-07-04 00:31:54 +1000

    db for heroku

--- modules/db/src/main/scala/so/paws/db/sorm/SormDbPlugin.scala
+++ modules/db/src/main/scala/so/paws/db/sorm/SormDbPlugin.scala
@@ -1,5 +1,7 @@
 package so.paws.db.sorm
 
+import java.net.URI
+
 import play.api.Play
 import sorm.{Entity, Instance, InitMode}
 import scala.reflect.runtime.universe
@@ -15,16 +17,18 @@ class SormDbPlugin(application: Application) extends DbPlugin[Instance] {
     application.configuration.getStringList("sorm.entities").get.foreach ( e =>
       entities ++= getEntities(application, e)
     )
-
-    object db extends Instance(
+    val dbUri = new URI(System.getenv("DATABASE_URL"))
+    val username = dbUri.getUserInfo().split(":")(0)
+    val password = dbUri.getUserInfo().split(":")(1)
+    new Instance(
       entities = entities,
-      url = Play.current.configuration.getString("db.default.url").getOrElse(""), // "jdbc:h2:./db/paws;AUTO_SERVER=TRUE",
-      user = "sa",
-      password = "",
+      url = "jdbc:postgresql://"+ dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath(),
+    //Play.current.configuration.getString("db.default.url").getOrElse(""), // "jdbc:h2:./db/paws;AUTO_SERVER=TRUE",
+      user = username,
+      password = password,
       initMode = InitMode.DropAllCreate,
       poolSize = 100
     )
-    db
   }
 
   def getEntities(application: Application, ref: String): Set[Entity] = {
