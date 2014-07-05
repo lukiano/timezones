5bcd990b5b611f4b90ff17b9770d14de033a02e6
Luciano Leggieri <lleggieri@atlassian.com>
2014-07-05 21:10:59 +1000

    trying to make it work on heroku

--- modules/db/src/main/scala/so/paws/db/sorm/SormDbPlugin.scala
+++ modules/db/src/main/scala/so/paws/db/sorm/SormDbPlugin.scala
@@ -2,11 +2,10 @@ package so.paws.db.sorm
 
 import java.net.URI
 
-import play.api.Play
+import play.api.{Logger, Play, Application}
 import sorm.{Entity, Instance, InitMode}
 import scala.reflect.runtime.universe
 import so.paws.db.DbPlugin
-import play.api.Application
 import scala.collection.JavaConversions._
 
 class SormDbPlugin(application: Application) extends DbPlugin[Instance] {
@@ -20,9 +19,11 @@ class SormDbPlugin(application: Application) extends DbPlugin[Instance] {
     Play.current.configuration.getString("sorm.url") match {
       case Some(url) =>
         val dbUri = new URI(url)
+        val urlString = "jdbc:postgresql://"+ dbUri.getHost + (if (dbUri.getPort == -1) "" else ':' + dbUri.getPort) + dbUri.getPath
+        Logger(classOf[SormDbPlugin]).info(urlString)
         new Instance(
           entities = entities,
-          url = "jdbc:postgresql://"+ dbUri.getHost + (if (dbUri.getPort == -1) "" else ':' + dbUri.getPort) + dbUri.getPath,
+          url = urlString,
           user = dbUri.getUserInfo.split(":")(0),
           password = dbUri.getUserInfo.split(":")(1),
           initMode = InitMode.DropAllCreate,
